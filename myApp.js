b31287ef4ea3b1609a6bf737649fa0d2e31126a4
Cristhian J. Martinez Lara <cristhian.martinezlara@gmail.com>
2023-11-10 15:33:24 -0600

    Older Mongoose Version Added

--- myApp.js
+++ myApp.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 
 // Connect to DB
 mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => console.log("DB Connected!"))
     .catch((err) => console.log(err));
 
