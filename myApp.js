4cc1087ea397ba543c8cb2e3e6ed7c8f316226e9
Cristhian J. Martinez Lara <cristhian.martinezlara@gmail.com>
2023-11-10 15:22:33 -0600

    MongoDB Connection Fixed

--- myApp.js
+++ myApp.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 // Connect to DB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("DB Connected!"))
+    .catch((err) => console.log(err));
 
 let Person;
 
