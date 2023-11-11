7a1c0c162f5ca9c8295b05c9fe3aea44cac7e616
Cristhian J. Martinez Lara <cristhian.martinezlara@gmail.com>
2023-11-10 18:22:43 -0600

    Find By Name

--- myApp.js
+++ myApp.js
@@ -19,11 +19,14 @@ const createManyPeople = (arrayOfPeople, done) => {
     Person.create(arrayOfPeople, (err, data) => {
         if (err) return done(err);
         done(null, data);
-    })
+    });
 };
 
 const findPeopleByName = (personName, done) => {
-    done(null /*, data*/);
+    Person.find({ name: personName }, (err, data) => {
+        if (err) return done(err);
+        done(null, data);
+    });
 };
 
 const findOneByFood = (food, done) => {
