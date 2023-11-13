39f3880eb4e27604e3a4d1277f5e7d522b8593e3
Cristhian J. Martinez Lara <cristhian.martinezlara@gmail.com>
2023-11-12 22:27:57 -0600

    headers request

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// Proxy
-app.set("trust proxy", true);
-
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
@@ -24,12 +21,10 @@ app.get("/", function (req, res) {
 
 // your first API endpoint...
 app.get("/api/whoami", function (req, res) {
-    
     res.json({
-        ipaddress: req.socket.remoteAddress,
-        ipaddressTestin: req.ip,
-        language: "",
-        software: "",
+        ipaddress: req.ip,
+        language: req.headers["accept-language"],
+        software: req.headers["user-agent"],
     });
 });
 
