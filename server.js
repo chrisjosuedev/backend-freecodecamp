f5542ee90c36d73a1e852af47d7f5d43c705665e
Cristhian J. Martinez Lara <cristhian.martinezlara@gmail.com>
2023-11-15 16:49:28 -0600

    File Uploaded

--- server.js
+++ server.js
@@ -3,14 +3,33 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
     res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+    if (req.file) {
+        const { originalname, mimetype, size } = req.file;
+        return res.status(200).json({
+            name: originalname,
+            type: mimetype,
+            size,
+        });
+    }
+    res.status(400).json({
+        msg: "No file received.",
+    });
 });
 
 app.listen(port, () => {
